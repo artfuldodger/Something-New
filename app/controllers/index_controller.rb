48b425b50fb5f2b8a979a5837621399a76512356
Jon Evans <jon@plantwars.com>
2010-12-31 17:06:35 -0600

    Adds top tags to the index page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,6 +2,7 @@ class IndexController < ApplicationController
   def index
     @newest_users = User.last(10)
     @todays_activities = Activity.where(:date => Date.today).limit(10)
+    @top_tags = Tag.group(:name).order('count(name) desc').limit(10)
     if current_user
       @my_activities_today = Activity.where(:date => Date.today, :user_id => current_user.id)
     end
