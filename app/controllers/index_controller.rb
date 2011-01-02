c576ecdd8910473216e8220dfd4e56716f8bb964
Jon Evans <jon@plantwars.com>
2011-01-01 20:02:58 -0600

    Order activities from new to old

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,7 @@
 class IndexController < ApplicationController
   def index
     @newest_users = User.order('id desc').last(10)
-    @todays_activities = Activity.where(:date => Date.today).limit(10)
+    @todays_activities = Activity.where(:date => Date.today).order('id desc').limit(10)
     @top_tags = Tag.group(:name).order('count(name) desc').limit(10)
     if current_user
       @my_activities_today = Activity.where(:date => Date.today, :user_id => current_user.id)
