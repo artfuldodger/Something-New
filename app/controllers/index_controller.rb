3e1ec46175822a3361f72d628ce8078823318d6c
Jon Evans <jon@plantwars.com>
2011-01-23 21:27:47 -0600

    Fall back to latest activities if none are present for today or yesterday

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,6 +3,7 @@ class IndexController < ApplicationController
     @newest_users = User.order('id desc').first(10)
     @todays_activities = Activity.where(:date => Date.today).order('id desc').limit(10)
     @yesterdays_activities = Activity.where(:date => Date.today-1).order('id desc').limit(10)
+    @latest_activities = Activity.order('id desc').limit(15)
     @top_tags = Tag.group('name COLLATE NOCASE').order('count(name) desc').limit(10)
     if current_user
       @my_activities_today = Activity.where(:date => Date.today, :user_id => current_user.id)
